a6c0fedd1758741a5d9f9ef8ed72933520ffc99a
Dilan <dilan@dilanxd.com>
2022-08-10 18:24:51 -0500

    update button dark mode visual bug

--- src/components/search/SearchButton.tsx
+++ src/components/search/SearchButton.tsx
@@ -13,7 +13,7 @@ function SearchButton({ action, children, active }: SearchButtonProps) {
       className={`flex-1 bg-${color}-200 dark:bg-gray-600 text-${color}-700 dark:text-${color}-300
                 font-medium p-1 rounded-lg opacity-60 transition-all duration-150
                 hover:opacity-100 active:bg-${color}-300 active:text-${color}-800 active:opacity-100
-                dark:active:bg-gray-500 dark:active:text-${color}-400`}
+                dark:active:bg-gray-500 dark:active:text-${color}-200`}
       onClick={() => action()}
     >
       {children}
