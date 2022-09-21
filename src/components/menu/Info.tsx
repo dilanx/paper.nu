4a5d07347cde8bb7f006e635d374c0c4a9a0ea8d
Dilan <dilan@dilanxd.com>
2022-09-20 20:00:11 -0500

    update name references

--- src/components/menu/Info.tsx
+++ src/components/menu/Info.tsx
@@ -10,11 +10,11 @@ class Info extends React.Component<InfoProps> {
   render() {
     return (
       <div
-        className={`flex items-center gap-2 justify-center mt-4 mb-2 px-4 py-2 text-center whitespace-nowrap border border-black
+        className={`flex items-center gap-2 justify-center mt-4 mb-2 px-4 py-2 text-center whitespace-nowrap border-2 border-black
                 dark:bg-gray-800 rounded-xl transition-all duration-300`}
       >
-        <PaperAirplaneIcon className="w-6 h-6" />
-        <p className="font-extralight drop-shadow-sm text-2xl">paper.nu</p>
+        <PaperAirplaneIcon className="w-6 h-6 drop-shadow-md" />
+        <p className="font-extralight drop-shadow-md text-2xl">paper.nu</p>
       </div>
     );
   }
