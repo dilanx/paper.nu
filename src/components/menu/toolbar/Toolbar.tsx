7cb3d1d56dbc980b57e8467993e6d114959bee64
Dilan <dilan@dilanxd.com>
2023-01-15 00:31:21 -0600

    improve toolbar responsiveness

--- src/components/menu/toolbar/Toolbar.tsx
+++ src/components/menu/toolbar/Toolbar.tsx
@@ -68,7 +68,7 @@ function Toolbar({
   }, [takeImage, switches]);
 
   return (
-    <div className="sticky top-0 z-30 flex w-full items-center justify-center gap-1 bg-white px-4 pt-4 pb-2 dark:bg-gray-800 lg:justify-end">
+    <div className="sticky top-0 z-30 flex w-full flex-col items-center justify-center gap-1 bg-white px-4 pt-4 pb-2 dark:bg-gray-800 md:flex-row">
       {activeItem && activeItem !== 'None' && (
         <div className="flex flex-1 items-center gap-2 font-semibold text-gray-400">
           {isSchedule ? (
@@ -79,99 +79,175 @@ function Toolbar({
           <p>{activeItem}</p>
         </div>
       )}
-      {isSchedule && (
+      <div className="flex gap-1">
+        {isSchedule && (
+          <ToolbarButton
+            theme={theme}
+            icon={MapIcon}
+            onClick={() => {
+              if (openMap) {
+                openMap();
+              }
+            }}
+          >
+            Campus map
+          </ToolbarButton>
+        )}
+        {isSchedule && (
+          <ToolbarButton
+            theme={theme}
+            icon={ArrowDownOnSquareIcon}
+            active={contextMenuData?.name === 'export'}
+            onClick={(x, y) => {
+              contextMenu({
+                name: 'export',
+                x,
+                y,
+                theme,
+                items: [
+                  {
+                    text: 'Export as image',
+                    icon: CameraIcon,
+                    onClick: () => {
+                      alert({
+                        title: 'Export schedule as image',
+                        icon: CameraIcon,
+                        message:
+                          'This will export your schedule as an image, which you can then share!',
+                        confirmButton: 'Download',
+                        color: 'sky',
+                        cancelButton: 'Cancel',
+                        action: () => setTakeImage(true),
+                      });
+                    },
+                  },
+                  {
+                    text: 'Export to calendar',
+                    icon: CalendarIcon,
+                    onClick: () => {
+                      const { validSections, invalidSections } =
+                        getSections(schedule);
+                      const noValidSections =
+                        Object.keys(validSections).length === 0;
+
+                      const invalidSectionMessage =
+                        Object.keys(invalidSections).length > 0
+                          ? `\n\nThe following sections cannot be added to the exported schedule because they don't have valid meeting time data: ${Utility.formatSections(
+                              invalidSections
+                            )}`
+                          : '';
+
+                      alert({
+                        title: 'Export schedule to calendar',
+                        icon: CalendarIcon,
+                        message: `This will export your schedule to an ICS file, which you can then import into your calendar app! Ensure that you set your time zone to central time (Chicago, United States).${invalidSectionMessage}`,
+                        textHTML: (
+                          <p className="my-2">
+                            Here are some instructions on how to import events
+                            into some popular calendar apps:{' '}
+                            <a href="https://support.apple.com/en-ca/guide/calendar/icl1023/mac">
+                              Apple Calendar
+                            </a>
+                            ,{' '}
+                            <a href="https://support.google.com/calendar/answer/37118?hl=en&co=GENIE.Platform%3DDesktop">
+                              Google Calendar
+                            </a>
+                            ,{' '}
+                            <a href="https://support.microsoft.com/en-us/office/import-calendars-into-outlook-8e8364e1-400e-4c0f-a573-fe76b5a2d379">
+                              Microsoft Outlook
+                            </a>
+                          </p>
+                        ),
+                        confirmButton: 'Download',
+                        disableConfirmButton: noValidSections
+                          ? 'Nothing to export'
+                          : undefined,
+                        color: 'red',
+                        cancelButton: 'Cancel',
+                        action: () => {
+                          toast.promise(exportScheduleAsICS(validSections), {
+                            loading: 'Exporting schedule...',
+                            success: 'Exported schedule',
+                            error: (res) => {
+                              console.error(res);
+                              return 'Failed to export schedule';
+                            },
+                          });
+                        },
+                      });
+                    },
+                  },
+                ],
+              });
+            }}
+          >
+            Export
+          </ToolbarButton>
+        )}
         <ToolbarButton
           theme={theme}
-          icon={MapIcon}
+          icon={ArrowTopRightOnSquareIcon}
           onClick={() => {
-            if (openMap) {
-              openMap();
-            }
+            alert({
+              title: 'Ready to share!',
+              message:
+                'All of your data is stored in the URL. When you make changes, the URL is updated to reflect them. Save it somewhere, or share with a friend!',
+              confirmButton: 'Copy to clipboard',
+              cancelButton: 'Close',
+              color: 'pink',
+              icon: ArrowTopRightOnSquareIcon,
+              textView: window.location.href,
+              action: () => {
+                navigator.clipboard.writeText(window.location.href);
+                toast.success('URL copied to clipboard');
+              },
+            });
           }}
         >
-          Campus map
+          Share
         </ToolbarButton>
-      )}
-      {isSchedule && (
-        <ToolbarButton
+        <ToolbarAccount
+          loading={loading}
           theme={theme}
-          icon={ArrowDownOnSquareIcon}
-          active={contextMenuData?.name === 'export'}
-          onClick={(x, y) => {
+          active={contextMenuData?.name === 'account'}
+          onClick={(x, y, signedIn) => {
+            if (!signedIn) {
+              Account.logIn();
+              return;
+            }
             contextMenu({
-              name: 'export',
+              name: 'account',
               x,
               y,
               theme,
               items: [
                 {
-                  text: 'Export as image',
-                  icon: CameraIcon,
+                  text: isSchedule ? 'Schedules' : 'Plans',
+                  icon: isSchedule ? CalendarIcon : RectangleStackIcon,
                   onClick: () => {
-                    alert({
-                      title: 'Export schedule as image',
-                      icon: CameraIcon,
-                      message:
-                        'This will export your schedule as an image, which you can then share!',
-                      confirmButton: 'Download',
-                      color: 'sky',
-                      cancelButton: 'Cancel',
-                      action: () => setTakeImage(true),
-                    });
+                    switches.set('tab', 'Plans');
                   },
                 },
                 {
-                  text: 'Export to calendar',
-                  icon: CalendarIcon,
+                  text: 'Manage account',
+                  icon: UserCircleIcon,
                   onClick: () => {
-                    const { validSections, invalidSections } =
-                      getSections(schedule);
-                    const noValidSections =
-                      Object.keys(validSections).length === 0;
-
-                    const invalidSectionMessage =
-                      Object.keys(invalidSections).length > 0
-                        ? `\n\nThe following sections cannot be added to the exported schedule because they don't have valid meeting time data: ${Utility.formatSections(
-                            invalidSections
-                          )}`
-                        : '';
-
+                    window.open('https://auth.dilanxd.com/account', '_blank');
+                  },
+                },
+                {
+                  text: 'Sign out',
+                  icon: ArrowRightOnRectangleIcon,
+                  onClick: () => {
                     alert({
-                      title: 'Export schedule to calendar',
-                      icon: CalendarIcon,
-                      message: `This will export your schedule to an ICS file, which you can then import into your calendar app! Ensure that you set your time zone to central time (Chicago, United States).${invalidSectionMessage}`,
-                      textHTML: (
-                        <p className="my-2">
-                          Here are some instructions on how to import events
-                          into some popular calendar apps:{' '}
-                          <a href="https://support.apple.com/en-ca/guide/calendar/icl1023/mac">
-                            Apple Calendar
-                          </a>
-                          ,{' '}
-                          <a href="https://support.google.com/calendar/answer/37118?hl=en&co=GENIE.Platform%3DDesktop">
-                            Google Calendar
-                          </a>
-                          ,{' '}
-                          <a href="https://support.microsoft.com/en-us/office/import-calendars-into-outlook-8e8364e1-400e-4c0f-a573-fe76b5a2d379">
-                            Microsoft Outlook
-                          </a>
-                        </p>
-                      ),
-                      confirmButton: 'Download',
-                      disableConfirmButton: noValidSections
-                        ? 'Nothing to export'
-                        : undefined,
-                      color: 'red',
+                      title: 'Logging out...',
+                      message: `Are you sure you want to log out? Make sure your changes are saved!`,
                       cancelButton: 'Cancel',
+                      confirmButton: 'Log out',
+                      color: 'rose',
+                      icon: ArrowRightOnRectangleIcon,
                       action: () => {
-                        toast.promise(exportScheduleAsICS(validSections), {
-                          loading: 'Exporting schedule...',
-                          success: 'Exported schedule',
-                          error: (res) => {
-                            console.error(res);
-                            return 'Failed to export schedule';
-                          },
-                        });
+                        Account.logOut();
                       },
                     });
                   },
@@ -179,82 +255,8 @@ function Toolbar({
               ],
             });
           }}
-        >
-          Export
-        </ToolbarButton>
-      )}
-      <ToolbarButton
-        theme={theme}
-        icon={ArrowTopRightOnSquareIcon}
-        onClick={() => {
-          alert({
-            title: 'Ready to share!',
-            message:
-              'All of your data is stored in the URL. When you make changes, the URL is updated to reflect them. Save it somewhere, or share with a friend!',
-            confirmButton: 'Copy to clipboard',
-            cancelButton: 'Close',
-            color: 'pink',
-            icon: ArrowTopRightOnSquareIcon,
-            textView: window.location.href,
-            action: () => {
-              navigator.clipboard.writeText(window.location.href);
-              toast.success('URL copied to clipboard');
-            },
-          });
-        }}
-      >
-        Share
-      </ToolbarButton>
-      <ToolbarAccount
-        loading={loading}
-        theme={theme}
-        active={contextMenuData?.name === 'account'}
-        onClick={(x, y, signedIn) => {
-          if (!signedIn) {
-            Account.logIn();
-            return;
-          }
-          contextMenu({
-            name: 'account',
-            x,
-            y,
-            theme,
-            items: [
-              {
-                text: isSchedule ? 'Schedules' : 'Plans',
-                icon: isSchedule ? CalendarIcon : RectangleStackIcon,
-                onClick: () => {
-                  switches.set('tab', 'Plans');
-                },
-              },
-              {
-                text: 'Manage account',
-                icon: UserCircleIcon,
-                onClick: () => {
-                  window.open('https://auth.dilanxd.com/account', '_blank');
-                },
-              },
-              {
-                text: 'Sign out',
-                icon: ArrowRightOnRectangleIcon,
-                onClick: () => {
-                  alert({
-                    title: 'Logging out...',
-                    message: `Are you sure you want to log out? Make sure your changes are saved!`,
-                    cancelButton: 'Cancel',
-                    confirmButton: 'Log out',
-                    color: 'rose',
-                    icon: ArrowRightOnRectangleIcon,
-                    action: () => {
-                      Account.logOut();
-                    },
-                  });
-                },
-              },
-            ],
-          });
-        }}
-      />
+        />
+      </div>
       {takeImage && (
         <div className="relative">
           <Schedule
