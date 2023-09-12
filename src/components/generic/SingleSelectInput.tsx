22879b36e4d5e0cb0fe393b6b26ddea6a2e34bcf
Dilan <dilan@dilanxd.com>
2023-09-11 17:55:45 -0700

    recolor select inputs

--- src/components/generic/SingleSelectInput.tsx
+++ src/components/generic/SingleSelectInput.tsx
@@ -27,17 +27,13 @@ function SingleSelectInput({
         >
           <input
             type="checkbox"
-            className={`h-4 w-4 appearance-none rounded-full
-              border-2 border-gray-400 checked:border-green-500
-              checked:bg-green-500 group-hover:border-gray-600
-              checked:group-hover:border-green-400
-              checked:group-hover:bg-green-400
-              group-active:border-green-300 checked:group-active:border-green-300
-              checked:group-active:bg-green-300
-              dark:border-gray-600 dark:checked:border-green-500
-              dark:group-hover:border-gray-400
-              dark:checked:group-hover:border-green-400 dark:group-active:border-green-300
-              dark:checked:group-active:border-green-300`}
+            className="h-4 w-4 appearance-none rounded-full border-2
+              border-gray-400 checked:border-black checked:bg-black
+              group-hover:border-gray-500 checked:group-hover:border-gray-700 checked:group-hover:bg-gray-700
+              group-active:border-gray-600 checked:group-active:border-gray-600 checked:group-active:bg-gray-600
+              dark:border-gray-600 dark:checked:border-white dark:checked:bg-white
+              dark:group-hover:border-gray-400 dark:checked:group-hover:border-gray-300 dark:checked:group-hover:bg-gray-300
+              dark:group-active:border-gray-300 dark:checked:group-active:border-gray-400 dark:checked:group-active:bg-gray-400"
             name={option}
             checked={value === option}
             onChange={(e) => {
