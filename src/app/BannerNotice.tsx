7db205aeedf785482355da3c8531b8ef5d5fbd7b
Dilan <dilan@dilanxd.com>
2023-11-28 16:20:59 -0600

    update banner notice

--- src/app/BannerNotice.tsx
+++ src/app/BannerNotice.tsx
@@ -1,20 +1,20 @@
 import { BannerData } from '../types/BaseTypes';
 
 const bn: BannerData = {
-  id: 'v3-instagram-2',
+  id: 'v3-instagram-3',
   gradient: 1,
   content: (
     <>
-      Version 3 is almost here.{' '}
+      Delaying v3 a bit bc I have too many assignments due that I should've
+      started earlier.{' '}
       <a
         href="https://www.instagram.com/paper.nu"
         target="_blank"
         rel="noreferrer"
         className="text-yellow-100 underline underline-offset-2 hover:text-yellow-300 hover:no-underline"
       >
-        Follow paper.nu on Instagram
+        Follow the insta, though.
       </a>{' '}
-      to stay updated! 🥺
     </>
   ),
 };
