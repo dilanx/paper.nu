0d4b9623da7a59ce56b00d15b10e4166fef1413a
Dilan Nair <dilanmail16@gmail.com>
2024-05-18 06:45:39 -0500

    Update index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 import './index.css';
-import App from './App';
+// import App from './App';
 import SiteNotice from './components/menu/SiteNotice';
 
 const container = document.getElementById('root');
