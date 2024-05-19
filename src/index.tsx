93b64328f0344225c4ad3754587462f3803a12d9
Dilan Nair <dilan@dilanxd.com>
2024-05-19 02:14:50 -0500

    Update index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,12 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 import './index.css';
-// import App from './App';
-import SiteNotice from './components/menu/SiteNotice';
+import App from './App';
 
 const container = document.getElementById('root');
 const root = createRoot(container!); // createRoot(container!) if you use TypeScript
 root.render(
   <React.StrictMode>
-    {/* <App /> */}
-    <SiteNotice />
+    <App />
   </React.StrictMode>
 );
