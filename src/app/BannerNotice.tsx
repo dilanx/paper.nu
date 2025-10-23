58aa6cd3e5435694df34bb434ac9f213af3bcc9e
Dilan <dilan@dilanxd.com>
2025-10-22 19:42:55 -0700

    remove banner

--- src/app/BannerNotice.tsx
+++ src/app/BannerNotice.tsx
@@ -1,23 +1,5 @@
 import { BannerData } from '@/types/BaseTypes';
 
-const bn: BannerData | null = {
-  id: 'discord-0195d54a-b7bb-73a4-9317-b7ade565a114',
-  gradient: 4,
-  content: (
-    <>
-      Connect with thousands of students and alumni on the{' '}
-      <a
-        href="https://discord.gg/aERJFBAhyP"
-        target="_blank"
-        className="rounded-sm  text-yellow-100 underline underline-offset-2 hover:bg-white/25 hover:no-underline"
-      >
-        Northwestern Discord server
-      </a>
-      !
-    </>
-  ),
-
-  //
-};
+const bn: BannerData | null = null;
 
 export default bn;
