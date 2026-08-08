480b1a619e4b2cfe26e09c4eaba4b1efd1d742a2
Dilan <dilan@dilanxd.com>
2026-08-07 22:45:58 -0700

    discord banner

--- src/app/BannerNotice.tsx
+++ src/app/BannerNotice.tsx
@@ -1,5 +1,22 @@
 import { BannerData } from '@/types/BaseTypes';
 
-const bn: BannerData | null = null;
+const bn: BannerData | null = {
+  id: 'discord-019fdfe2-4263-7083-b40f-c0ecb1715852',
+  gradient: 4,
+  theme: 'dark',
+  content: (
+    <>
+      Connect with thousands of students and alumni on the{' '}
+      <a
+        href="https://discord.com/servers/northwestern-655629737888055317"
+        target="_blank"
+        className="rounded-sm  text-yellow-100 underline underline-offset-2 hover:bg-white/25 hover:no-underline"
+      >
+        Northwestern Discord server
+      </a>
+      !
+    </>
+  ),
+};
 
 export default bn;
